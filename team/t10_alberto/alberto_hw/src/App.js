17ffd2e838227e6ad3c472868b77b8af2b402b76
Alberto Huang <ahuanghe@umail.ucsb.edu>
2021-10-07 15:57:31 -0700

    Alberto's Hello World App with frontend and backend

--- team/t10_alberto/alberto_hw/src/App.js
+++ team/t10_alberto/alberto_hw/src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Hello World! </h1>
-        <p>This is Alberto saying hello!</p>
+class App extends Component {
+  state = {
+      data: null
+    };
+  
+    componentDidMount() {
+      this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    }
+      // fetching the GET route from the Express server which matches the GET route from server.js
+    callBackendAPI = async () => {
+      const response = await fetch('/express_backend');
+      const body = await response.json();
+  
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      return body;
+    };
 
-      </header>
-    </div>
-  );
-}
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1> Hello World! </h1>
+            <p>This is Alberto saying hello!</p>
+
+          </header>
+        </div>
+      );
+    }
+  }
 
 export default App;
