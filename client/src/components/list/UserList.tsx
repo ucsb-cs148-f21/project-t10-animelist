9f1d42bd1bff6c386cd65e7f56c5964039c0f236
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-10-15 12:26:12 -0700

    Update styling in UserList component

--- client/src/components/list/UserList.tsx
+++ client/src/components/list/UserList.tsx
@@ -1,27 +1,19 @@
-import { Avatar, Heading, VStack } from '@chakra-ui/react';
-import * as React from 'react';
-import { User } from '../../generated/graphql';
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import * as React from "react";
+import { User } from "../../generated/graphql";
 
 interface Props {
-	user: Pick<User, "username">
-};
+  user: Pick<User, "username">;
+}
 
 const UserList: React.FC<Props> = ({ user }) => {
-	return (
-		<VStack
-			w="full"
-			p={6}
-		>
-			<Avatar
-        size={"xl"}
-        name={user.username}
-      />	
+  return (
+    <VStack w="full" p={6}>
+      <Avatar size={"xl"} name={user.username} />
 
-			<Heading>
-				Placeholder list for {user.username}
-			</Heading>
-		</VStack>
-	)
+      <Heading>Placeholder list for {user.username}</Heading>
+    </VStack>
+  );
 };
 
 export default UserList;
