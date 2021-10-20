68abc7e8439c966edab3dfe8423d0082e387ec25
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-10-19 22:23:52 -0700

    Add user list to user class and schema

--- server/src/main/java/com/github/animelist/animelist/model/user/UserListEntry.java
+++ server/src/main/java/com/github/animelist/animelist/model/user/UserListEntry.java
@@ -1,3 +1,9 @@
 package com.github.animelist.animelist.model.user;
 
-public record UserListEntry(int mediaID, double rating) {}
+/**
+ * Represents a single entry in a user's anime list.
+ * If the user has given a rating for this entry, the rated field is true
+ * and the rating stored in the rating field. Otherwise, the rated field is
+ * false and the value of the rating field is meaningless.
+ */
+public record UserListEntry(int mediaID, boolean rated, double rating) {}
