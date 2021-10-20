c43c074a0d4ba77cf342dbb737350d998949ef20
zihaoxu <zihaoxu@ucsb.edu>
2021-10-20 15:03:42 -0700

    change the maxwidth

--- client/src/components/list/UserList.tsx
+++ client/src/components/list/UserList.tsx
@@ -1,6 +1,4 @@
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
-import * as React from "react";
-import { User } from "../../generated/graphql";
 import {
   Table,
   Thead,
@@ -12,6 +10,8 @@ import {
   TableCaption,
 } from "@chakra-ui/react"
 import { Button, ButtonGroup} from "@chakra-ui/react"
+import * as React from "react";
+import { User } from "../../generated/graphql";
 
 interface Props {
   user: Pick<User, "username">;
@@ -19,11 +19,10 @@ interface Props {
 
 const UserList: React.FC<Props> = ({ user }) => {
   return (
-    <VStack w="full" p={6}>
+    <VStack width="full" p={6} maxWidth = "6xl">
       <Avatar size={"xl"} name={user.username} />
 
       <Heading>Placeholder list for {user.username}</Heading>
-
     <Table>
       <TableCaption>This is my animelist</TableCaption>
       <Thead>
@@ -49,12 +48,9 @@ const UserList: React.FC<Props> = ({ user }) => {
     </Table>
 
     <Button colorScheme="blue">Add Anime</Button>
+
     </VStack>
-    
   );
 };
-
-
-
 
 export default UserList;
