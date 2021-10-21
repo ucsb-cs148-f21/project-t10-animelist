7e72f74fcac4427dbaae0a41e5e938354b881c6f
jvoucsb <jvo@umail.ucsb.edu>
2021-10-21 02:12:31 -0700

    jv - change to use stack instead of container

--- client/src/pages/search.tsx
+++ client/src/pages/search.tsx
@@ -1,18 +1,18 @@
-import { Container } from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import * as React from 'react';
 import SearchForm from '../components/forms/SearchForm';
 
 
 const Search: React.FC<{}> = () => {
   return (
-    <Container
+    <Stack
       py={{ base: 10 }}
       px={{ base: 10, xl: 0 }}
       width="full"
       maxW="6xl"
     >
       <SearchForm />
-    </Container>
+    </Stack>
   );
 };
 
