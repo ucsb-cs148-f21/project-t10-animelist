8a37a7b8b40ce3bcd87253bf6ae4773783fe6fe5
christopherbarnett <christopherbarnett@umail.ucsb.edu>
2021-10-21 21:44:54 -0700

    cb - created searchresult component

---
+++ client/src/components/result/SearchResult.tsx
@@ -0,0 +1,43 @@
+import { HStack, Stack, Badge, Button, Image, Text, Heading } from '@chakra-ui/react';
+import * as React from 'react';
+
+interface SearchResultProps {
+    anime: any
+}
+
+const SearchResult: React.FC<SearchResultProps> = ({ anime }) => {
+    return (
+        <HStack key={anime.id}>
+            <Image src={anime.coverImage.medium} />
+            <Stack
+                height="100%"
+                alignItems="flex-start"
+            >
+                <Heading size={"sm"}>{anime.title.romaji}</Heading>
+                <HStack
+                    alignItems="center"
+                    flexWrap="wrap"
+                >
+                    <Text>Genres:</Text>
+                    {
+                        anime.genres.map(genre => (
+                            <Badge
+                                key={genre}
+                            >
+                                {genre}
+                            </Badge>
+                        ))
+                    }
+                </HStack>
+                <div style={{ flexGrow: 1 }} />
+                <Button
+                    size={"sm"}
+                >
+                    Add Anime
+                </Button>
+            </Stack>
+        </HStack>
+    );
+};
+
+export default SearchResult;
