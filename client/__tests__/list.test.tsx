f0809f8bf9420c3a5f05243e6c1030268f22508e
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-11-03 17:01:44 -0700

    start adding component tests for list page

---
+++ client/__tests__/list.test.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import List from '../src/pages/list';
+import { render, screen } from '@testing-library/react';
+import { setupServer, SetupServerApi } from 'msw/node';
+import { graphql } from 'msw';
+import { ANILIST_GRAPHQL_ENDPOINT } from '../src/utils/createApolloAnilist';
+import router from 'next/router';
+
+var server: SetupServerApi;
+beforeAll(() => {
+  const anilist = graphql.link(ANILIST_GRAPHQL_ENDPOINT);
+
+  server = setupServer(
+    graphql.query('UserList', (req, res, ctx) => {
+      return res(
+        ctx.data({
+          me: {
+            userList: [
+              {mediaID: 123, rated: true, rating: 8.4},
+              {mediaID: 999, rated: false, rating: 1.1}
+            ]
+          }
+        })
+      );
+    }),
+
+    anilist.query('FetchAnimeInfo', (req, res, ctx) => {
+      const { ids } = req.variables;
+
+      return res(
+        ctx.data({
+          Page: {
+            media: [ids.map(id => {
+              return {
+                id: id,
+                title: {
+                  romaji: "Dragon Ball Z"
+                },
+                coverImage: {
+                  medium: "https://via.placeholder.com/230x320"
+                }
+              };
+            })]
+          }
+        })
+      );
+    })
+  );
+
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+describe('User list', () => {
+  it('redirects to login page when not logged in', () => {
+    server.use(
+      graphql.query('UserList', (req, res, ctx) => {
+        return res(ctx.data(null));
+      })
+    );
+    jest.mock('next/router', () => {
+      push: jest.fn()
+    });
+
+    render(<List />);
+
+    expect(router.push).toHaveBeenCalledWith("/login");
+  });
+
+  it('displays two rows given two user list entries', () => {
+    render(<List />);
+
+    const table = screen.getByRole('table');
+    expect(table.firstChild).toHaveTextContent('Dragon Ball Z');
+  });
+});
