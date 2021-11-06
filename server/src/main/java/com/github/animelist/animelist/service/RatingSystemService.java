6d9a8afe0d80bb1ef6a610df73a319085f480a0e
jvoucsb <jvo@umail.ucsb.edu>
2021-11-05 17:08:24 -0700

    jv - add get rating system by id in service class

--- server/src/main/java/com/github/animelist/animelist/service/RatingSystemService.java
+++ server/src/main/java/com/github/animelist/animelist/service/RatingSystemService.java
@@ -1,21 +1,30 @@
 package com.github.animelist.animelist.service;
 
 import com.github.animelist.animelist.model.ratingsystem.RatingSystem;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class RatingSystemService {
 
-    private MongoTemplate mongoTemplate;
+    private final MongoTemplate mongoTemplate;
 
     @Autowired
     public RatingSystemService(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
     }
 
-    public void insertRatingSystem(final RatingSystem ratingSystem) {
-        mongoTemplate.insert(ratingSystem);
+    public RatingSystem createRatingSystem(final RatingSystem ratingSystem) {
+        return mongoTemplate.insert(ratingSystem);
+    }
+
+    public Optional<RatingSystem> getRatingSystem(final String id) {
+        return Optional.ofNullable(mongoTemplate.findById(new ObjectId(id), RatingSystem.class));
     }
 }
