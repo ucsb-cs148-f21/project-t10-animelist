9db934839ce70a5640f492ab0b852f396d81679f
jvoucsb <jvo@umail.ucsb.edu>
2021-11-09 03:11:03 -0800

    jv - add userlistservice methods, deprecate old classes

--- server/src/main/java/com/github/animelist/animelist/service/UserListService.java
+++ server/src/main/java/com/github/animelist/animelist/service/UserListService.java
@@ -2,12 +2,20 @@ package com.github.animelist.animelist.service;
 
 import com.github.animelist.animelist.entity.User;
 import com.github.animelist.animelist.model.input.UserListEntryInput;
+import com.github.animelist.animelist.model.userlist.UserList;
+import com.github.animelist.animelist.model.userlist.UserListItem;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+import static com.github.animelist.animelist.util.MongoUtil.verifyOneUpdated;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @Component
 public class UserListService {
@@ -19,6 +27,41 @@ public class UserListService {
         this.mongoTemplate = mongoTemplate;
     }
 
+    public UserList createUserList(final UserList userList) {
+        return mongoTemplate.insert(userList);
+    }
+
+    public Optional<UserList> getUserList(final String listId) {
+        return Optional.ofNullable(mongoTemplate.findById(new ObjectId(listId), UserList.class));
+    }
+
+    public boolean addItem(final String listId, final String ownerId, final UserListItem item) {
+        final var query = getListQuery(listId, ownerId);
+
+        final Update update = new Update();
+        update.push("items").value(item);
+
+        return verifyOneUpdated(mongoTemplate.updateFirst(query, update, UserList.class));
+    }
+
+    public boolean updateItem(final String listId, final String ownerId, final UserListItem item) {
+        final var query = getListQuery(listId, ownerId);
+        // query.addCriteria(where("items.mediaID").is(item.getMediaID()));
+
+        final Update update = new Update();
+        // update.set("items.$.rating", item.getRating());
+
+        return verifyOneUpdated(mongoTemplate.updateFirst(query, update, UserList.class));
+    }
+
+    private Query getListQuery(final String listId, final String ownerId) {
+        final Query query = new Query();
+        query.addCriteria(where("_id").is(listId).and("ownerId").is(ownerId));
+
+        return query;
+    }
+
+    @Deprecated
     public boolean updateUserListEntry(final String userId, final UserListEntryInput entry) {
         final Query query = new Query();
         query.addCriteria(Criteria.where("_id").is(userId).and("userList.mediaID").is(entry.mediaID()));
