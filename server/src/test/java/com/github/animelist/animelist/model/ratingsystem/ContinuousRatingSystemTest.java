38c6c81a3d21c4c9f74dfa063d049c7a69afd777
jvoucsb <jvo@umail.ucsb.edu>
2021-11-10 20:40:47 -0800

    jv - continuous rating system unit tests

---
+++ server/src/test/java/com/github/animelist/animelist/model/ratingsystem/ContinuousRatingSystemTest.java
@@ -0,0 +1,86 @@
+package com.github.animelist.animelist.model.ratingsystem;
+
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class ContinuousRatingSystemTest {
+
+    @Test
+    void createContinuousRatingSystemBuilder_happy() {
+        final var builder = ContinuousRatingSystem.builder()
+                .name("Test")
+                .ownerId(new ObjectId())
+                .size(10)
+                .offset(1)
+                .subRatings(Collections.singletonList(SubRating.builder().id(0).name("score").weight(1f).build()));
+
+        assertDoesNotThrow(builder::build);
+    }
+
+    @Test
+    void createContinuousRatingSystemBuilder_badOffset() {
+        final var builder = ContinuousRatingSystem.builder()
+                .name("Test")
+                .ownerId(new ObjectId())
+                .size(10)
+                .offset(-5)
+                .subRatings(Collections.singletonList(SubRating.builder().id(0).name("score").weight(1f).build()));
+
+        assertThrows(IllegalArgumentException.class, builder::build);
+    }
+
+    @Test
+    void createContinuousRatingSystemBuilder_badSize() {
+        final var builder = ContinuousRatingSystem.builder()
+                .name("Test")
+                .ownerId(new ObjectId())
+                .size(-5)
+                .offset(1)
+                .subRatings(Collections.singletonList(SubRating.builder().id(0).name("score").weight(1f).build()));
+
+        assertThrows(IllegalArgumentException.class, builder::build);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = { "", "  ", "\n", "\t", "tj\n" })
+    void createContinuousRatingSystemBuilder_badName(final String badName) {
+        final var builder = ContinuousRatingSystem.builder()
+                .name(badName)
+                .ownerId(new ObjectId())
+                .size(10)
+                .offset(1)
+                .subRatings(Collections.singletonList(SubRating.builder().id(0).name("score").weight(1f).build()));
+
+        assertThrows(IllegalArgumentException.class, builder::build);
+    }
+
+    @ParameterizedTest
+    @ValueSource(floats = { 0.2f,  0.90f, 1.01f, 0.98f, 2f})
+    void createContinuousRatingSystemBuilder_badWeightSum(final float badSubratingWeight) {
+        final var builder = ContinuousRatingSystem.builder()
+                .name("Test")
+                .ownerId(new ObjectId())
+                .size(10)
+                .offset(1)
+                .subRatings(asList(
+                        SubRating.builder().id(0).name("score").weight(badSubratingWeight).build(),
+                        SubRating.builder().id(1).name("score 2").weight(badSubratingWeight).build()
+                ));
+
+        assertThrows(IllegalArgumentException.class, builder::build);
+    }
+
+}
