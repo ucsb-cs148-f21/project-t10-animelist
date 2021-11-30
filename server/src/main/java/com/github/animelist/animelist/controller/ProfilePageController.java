3bff0de73a1db105f4b1357b8a221302de379df4
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-11-29 17:03:13 -0800

    rt - start work on profile page blocks mutation

---
+++ server/src/main/java/com/github/animelist/animelist/controller/ProfilePageController.java
@@ -0,0 +1,32 @@
+package com.github.animelist.animelist.controller;
+
+import java.util.List;
+
+import com.github.animelist.animelist.model.input.profilepage.ProfilePageInput;
+import com.github.animelist.animelist.model.profilepage.Block;
+import com.github.animelist.animelist.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+public class ProfilePageController {
+    private final UserService userService;
+
+    @Autowired
+    public ProfilePageController(
+        final UserService userService) {
+
+        this.userService = userService;
+    }
+
+    @MutationMapping
+    @PreAuthorize("isAuthenticated()")
+    public List<List<Block>> updateProfilePageBlocks(
+        @Argument("input") final ProfilePageInput input) {
+
+        userService.setProfilePageBlocks(input.blocks());
+        return profilePageService.getProfilePageBlocks();
+    }
+}
---
+++ server/src/main/java/com/github/animelist/animelist/model/input/profilepage/ProfilePageInput.java
@@ -0,0 +1,6 @@
+package com.github.animelist.animelist.model.input.profilepage;
+
+import java.util.List;
+import com.github.animelist.animelist.model.profilepage.Block;
+
+public record ProfilePageInput(List<List<Block>> blocks) {}
