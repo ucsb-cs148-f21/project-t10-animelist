58e8d6712310569069a1fac2183452da53cf48d9
Rowan-James Tran <rowanjames.tran@gmail.com>
2021-12-03 19:57:40 -0800

    rt - add buttons to lists page

--- client/src/pages/list.tsx
+++ client/src/pages/list.tsx
@@ -1,5 +1,5 @@
 import { ArrowRightIcon } from "@chakra-ui/icons";
-import { Button, Heading, HStack, Link, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Heading, HStack, Link, Text, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/dist/client/router";
 import * as React from "react";
 import { useMeQuery } from "../generated/graphql";
@@ -18,26 +18,41 @@ const List: React.FC<{}> = () => {
   }
 
   return (
-    <VStack py={{ base: 10 }} width="sm">
+    <VStack py={{ base: 10 }} width="sm" spacing={8}>
       <Heading>Your Lists</Heading>
-      {
-        data.me.userLists && data.me.userLists.length !== 0 &&
-        data.me.userLists.map((userlist) => (
-          <Button 
-            key={userlist.id}
-            width="full" 
-            maxW="sm" 
-            onClick={() => router.push(`list/${userlist.id}`)}
-          >
-            <HStack width="full" justifyContent="space-between">
-              <Text>
-                {userlist.name}
-              </Text>
-              <ArrowRightIcon />
-            </HStack>
-          </Button>
-        ))
-      }
+
+      <ButtonGroup
+        width="100%"
+        justifyContent='center'
+      >
+        <Link href="/createratingsystem">
+          <Button colorScheme="blue">Create Rating System</Button>
+        </Link>
+        <Link href="/createlist">
+          <Button colorScheme="blue">Create a List</Button>
+        </Link>
+      </ButtonGroup>
+
+      <VStack width="100%" spacing={2}>
+        {
+          data.me.userLists && data.me.userLists.length !== 0 &&
+          data.me.userLists.map((userlist) => (
+            <Button 
+              key={userlist.id}
+              width="full" 
+              maxW="sm" 
+              onClick={() => router.push(`list/${userlist.id}`)}
+            >
+              <HStack width="full" justifyContent="space-between">
+                <Text>
+                  {userlist.name}
+                </Text>
+                <ArrowRightIcon />
+              </HStack>
+            </Button>
+          ))
+        }
+      </VStack>
     </VStack>
   );
 };
